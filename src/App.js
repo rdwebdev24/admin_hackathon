d19ccfa44fa0dccd2afb045459777ebb9c6057e5
ASB1704 <singhanurag17042002@gmail.com>
2022-10-22 00:12:08 +0530

    Hogaya vai authority sign in

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import * as React from "react";
 import { Admin , ListGuesser , Resource} from 'react-admin';
 import { dataProvider } from "./Components/DataProvider";
+import { Signin } from "./Components/Signin/Signin";
 import {WasteData} from './Components/WasteData/WasteData'
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => (
-    <Admin dataProvider={dataProvider}>
-        <Resource name="task" list={WasteData} />
-    </Admin>
+    // <Admin dataProvider={dataProvider}>
+    //     <Resource name="task" list={WasteData} />
+    // </Admin>
+    <div className="app">
+    <Signin/>
+    </div>
 );
 
 export default App;
